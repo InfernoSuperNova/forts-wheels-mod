d84311303c92a07c978bc2e322744d2adf836417
Gxaps <36036803+InfernoSuperNova@users.noreply.github.com>
2023-05-06 09:03:36 +1200

    Updated power and max speed

--- scripts/effects.lua
+++ scripts/effects.lua
@@ -10,12 +10,9 @@ function WheelSmoke()
             local nodeA = GetDevicePlatformA(deviceId)
             local nodeB = GetDevicePlatformB(deviceId)
             local velocity = NodeVelocity(nodeA)
-            BetterLog(velocity)
             local velocityMag = VecMagnitude(velocity)
-            BetterLog(velocityMag)
             if velocityMag > 50 then
                 local pos = OffsetPerpendicular(NodePosition(nodeA), NodePosition(nodeB), WheelSuspensionHeight + WheelRadius)
-                BetterLog("e")
                 SpawnEffect(path .. "/effects/smoke_poof.lua", pos)
                 
             end
