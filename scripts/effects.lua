579050c46ca5aae8514d348b5609d0db009d4316
Gxaps <36036803+InfernoSuperNova@users.noreply.github.com>
2023-05-06 09:51:49 +1200

    i forgor

--- scripts/effects.lua
+++ scripts/effects.lua
@@ -4,18 +4,18 @@ function UpdateEffects()
 end
 
 function WheelSmoke()
-    for structure, wheels in pairs(data.wheelsTouchingGround) do
-        for deviceId, _ in pairs(wheels) do
+    -- for structure, wheels in pairs(data.wheelsTouchingGround) do
+    --     for deviceId, _ in pairs(wheels) do
             
-            local nodeA = GetDevicePlatformA(deviceId)
-            local nodeB = GetDevicePlatformB(deviceId)
-            local velocity = NodeVelocity(nodeA)
-            local velocityMag = VecMagnitude(velocity)
-            if velocityMag > 50 then
-                local pos = OffsetPerpendicular(NodePosition(nodeA), NodePosition(nodeB), WheelSuspensionHeight + WheelRadius)
-                SpawnEffect(path .. "/effects/smoke_poof.lua", pos)
+    --         local nodeA = GetDevicePlatformA(deviceId)
+    --         local nodeB = GetDevicePlatformB(deviceId)
+    --         local velocity = NodeVelocity(nodeA)
+    --         local velocityMag = VecMagnitude(velocity)
+    --         if velocityMag > 50 then
+    --             local pos = OffsetPerpendicular(NodePosition(nodeA), NodePosition(nodeB), WheelSuspensionHeight + WheelRadius)
+    --             SpawnEffect(path .. "/effects/smoke_poof.lua", pos)
                 
-            end
-        end
-    end
+    --         end
+    --     end
+    -- end
 end
