429c36bf4b005cbe03a3d71f74a3e40e763a5503
SamsterBirdies <71206056+SamsterBirdies@users.noreply.github.com>
2023-05-08 23:04:36 -0700

    add code for sound implementation (not enabled)
    code is implemented but commented out (has no effect yet).
    currently only works for coop.
    todo:
    make it work for tdm.
    only spawn effects for 1 engine per structure to avoid polyphony issues.

--- scripts/effects.lua
+++ scripts/effects.lua
@@ -1,8 +1,15 @@
 function UpdateEffects()
 
     WheelSmoke()
+    EngineSoundUpdate()
 end
-
+function InitializeEffects()
+	EffectsList =
+    --keeps track of existing effect ids
+	{
+		["engine"] = {},
+	}
+end
 function WheelSmoke()
     -- for structure, wheels in pairs(data.structures) do
     --     for deviceId, _ in pairs(wheels) do
@@ -18,4 +25,54 @@ function WheelSmoke()
     --         end
     --     end
     -- end
+end
+
+function EngineSoundUpdate()
+    --Engine sound script to run on Update()
+    --[[
+    if JustJoined then
+        EngineSoundOnJoin()
+    end
+    DebugLog(data.throttles)
+    for structureIndex, engine in pairs(data.throttles) do
+        local rpm = math.abs((engine.x - 274) * 25)
+        DebugLog(EffectsList)
+        for engine, effect in pairs(EffectsList.engine) do
+            DebugLog(GetDeviceStructureId(tonumber(engine)))
+            DebugLog(GetStructureId(structureIndex - 1))
+            if GetDeviceStructureId(tonumber(engine)) == GetStructureId(structureIndex - 1) then
+                SetAudioParameter(effect, "rpm", rpm)
+                SetEffectPosition(effect, GetDevicePosition(tonumber(engine)))
+            end
+        end
+    end]]
+end
+function EngineSoundAdd(saveName, deviceId)
+    --[[
+    --attaches an effect to a new engine device
+    if saveName == ControllerSaveName then
+        local id = SpawnEffect(path .. "/effects/engine_loop.lua", GetDevicePosition(deviceId))
+        EffectsList.engine[tostring(deviceId)] = id
+    end]]
+end
+function EngineSoundRemove(saveName, deviceId)
+    --[[
+    --removes an effect when an engine device is removed
+    if saveName == ControllerSaveName then
+        CancelEffect(EffectsList.engine[tostring(deviceId)])
+        EffectsList.engine[tostring(deviceId)] = nil
+    end]]
+end
+function EngineSoundOnJoin()
+    --[[
+    --attaches effects to engines upon joining
+    for side = 1, 2 do
+        local count = GetDeviceCountSide(side)
+        for index = 0, count do
+            local id = GetDeviceIdSide(side, index)
+            if GetDeviceType(id) == ControllerSaveName then
+                EngineSoundAdd(ControllerSaveName, id)
+            end
+        end
+    end]]
 end
