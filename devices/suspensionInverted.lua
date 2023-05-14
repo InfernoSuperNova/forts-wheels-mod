e55b992fce5d5f5d7e55059fd5bfb9ee8652c6b5
SamsterBirdies <71206056+SamsterBirdies@users.noreply.github.com>
2023-05-14 15:43:45 -0700

    Simplify suspension code.
    makes future changes easier.

--- devices/suspensionInverted.lua
+++ devices/suspensionInverted.lua
@@ -1,51 +1,5 @@
--- fort wars device file
-
-ConstructEffect = "effects/device_construct.lua"
-CompleteEffect = "effects/device_complete.lua"
-DestroyEffect = "effects/device_explode.lua"
-Scale = 1
-SelectionWidth = 25
-SelectionHeight = 25
-SelectionOffset = { 0.0, 25.5 }
-Mass = 375.0
-HitPoints = 250.0
-DestroyProjectile = nil
-EnergyProductionRate = 0.0
-MetalProductionRate = 0.0
-EnergyStorageCapacity = 0.0
-MetalStorageCapacity = 0.0
-MinWindEfficiency = 1
-MaxWindHeight = 0
-MaxRotationalSpeed = 0
-DrawBracket = false
-DrawBehindTerrain = true
-NoReclaim = false
-TeamOwned = true
-BlockPenetration = true
-
-dofile("effects/device_smoke.lua")
-SmokeEmitter = StandardDeviceSmokeEmitter
-
-
-
-Root =
-{
-	Name = "suspension",
-	Angle = 180,
-	Pivot = { 0, -0.36 },
-	PivotOffset = { 0, 0 },
-	Sprite = "suspension-base",
-    Scale = 1.3,
-    UserData = 0,
-
-	ChildrenInFront =
-	{
-        -- {
-		-- 	Name = "Wheel",
-		-- 	Pivot = { 0.0, 0.5 },
-		-- 	Sprite = "wheel",
-		-- 	UserData = 100,
-		-- 	Visible = false,
-		-- },
-	},
-}
+dofile(path .. "/devices/suspension.lua")
+Sprites = {}
+SelectionOffset = { 0.0, -25.5 }
+Root.Angle = 180
+Root.Pivot = {0, -0.36}
