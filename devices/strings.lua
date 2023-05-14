4a3f0b1379fc1b66c57bc5bc05811f3337af1ddd
Gxaps <36036803+InfernoSuperNova@users.noreply.github.com>
2023-05-15 00:39:38 +1200

    Gearbox strings

--- devices/strings.lua
+++ devices/strings.lua
@@ -9,4 +9,8 @@ Merge(Device,
     engine = L"Engine",
     engineTip2 = L"Powers the wheels. Can place multiple engines for more torque.",
     engineTip3 = L"Throttle can be adjusted when selected. Can be control grouped.",
+
+    gearbox = L"Transmission",
+    gearboxTip2 = L"Gears for the wheels. Place more to increase speed at the cost of torque.",
+    gearboxTip3 = L"If you're struggling up hills, delete them.",
 })
