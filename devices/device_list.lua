a4e2e90cab41cbc278155979b1c7f85488afc507
SamsterBirdies <71206056+SamsterBirdies@users.noreply.github.com>
2023-05-14 17:37:18 -0700

    Update icons
    thx bryansupper for wheel icons
    
    Update all icons

--- devices/device_list.lua
+++ devices/device_list.lua
@@ -1,47 +1,3 @@
-table.insert(Sprites, ButtonSprite("hud-suspension-icon", "HUD/HUD-suspension", nil, ButtonSpriteBottom, nil, nil, path))
-table.insert(Sprites, DetailSprite("hud-detail-suspension", "suspension", path))
-local suspension = 
-{
-    SaveName = "suspension",
-    FileName = path .. "/devices/suspension.lua",
-    Icon = "hud-suspension-icon",
-    Detail = "hud-detail-suspension",
-    BuildTimeComplete = 15,
-    ScrapPeriod = 8,
-    MetalCost = 150,
-    EnergyCost = 1000,
-    MetalRepairCost = 25,
-    EnergyRepairCost = 500,
-    MetalReclaimMin = 0,
-    MetalReclaimMax = 0,
-    EnergyReclaimMin = 0,
-    EnergyReclaimMax = 0,
-    MaxUpAngle = 90,
-    BuildOnGroundOnly = false,
-    SelectEffect = "ui/hud/devices/ui_devices",
-    HasDummy = false,
-}
-
-
-
-InsertDeviceBehind("sandbags", suspension)
-
-
-
-
-
-table.insert(Sprites, ButtonSprite("hud-suspensionInverted-icon", "HUD/HUD-suspension", nil, ButtonSpriteBottom, nil, nil, path))
-table.insert(Sprites, DetailSprite("hud-detail-suspensionInverted", "suspension", path))
-local suspension2 = DeepCopy(suspension)
-
-if suspension2 then
-    suspension2.SaveName = "suspensionInverted"
-    suspension2.FileName = path .. "/devices/suspensionInverted.lua"
-end
-
-InsertDeviceBehind("suspension", suspension2)
-
-
 table.insert(Sprites, ButtonSprite("hud-engine-icon", "HUD/HUD-engine", nil, ButtonSpriteBottom, nil, nil, path))
 table.insert(Sprites, DetailSprite("hud-detail-engine", "engine", path))
 local engine = 
@@ -67,16 +23,18 @@ local engine =
     ClaimsStructure = true,
 }
 
-
-
 InsertDeviceBehind("sandbags", engine)
 
+
+
+table.insert(Sprites, ButtonSprite("hud-gearbox-icon", "HUD/HUD-gearbox", nil, ButtonSpriteBottom, nil, nil, path))
+table.insert(Sprites, DetailSprite("hud-detail-gearbox", "gearbox", path))
 local gearbox = 
 {
     SaveName = "gearbox",
     FileName = path .. "/devices/gearbox.lua",
-    Icon = "hud-engine-icon",
-    Detail = "hud-detail-engine",
+    Icon = "hud-gearbox-icon",
+    Detail = "hud-detail-gearbox",
     BuildTimeComplete = 15,
     ScrapPeriod = 8,
     MetalCost = 150,
@@ -94,9 +52,51 @@ local gearbox =
     ClaimsStructure = true,
 }
 
-
-
 InsertDeviceBehind("engine", gearbox)
+
+
+
+
+table.insert(Sprites, ButtonSprite("hud-suspension-icon", "HUD/HUD-suspension", nil, ButtonSpriteBottom, nil, nil, path))
+table.insert(Sprites, DetailSprite("hud-detail-suspension", "suspension", path))
+local suspension = 
+{
+    SaveName = "suspension",
+    FileName = path .. "/devices/suspension.lua",
+    Icon = "hud-suspension-icon",
+    Detail = "hud-detail-suspension",
+    BuildTimeComplete = 15,
+    ScrapPeriod = 8,
+    MetalCost = 150,
+    EnergyCost = 1000,
+    MetalRepairCost = 25,
+    EnergyRepairCost = 500,
+    MetalReclaimMin = 0,
+    MetalReclaimMax = 0,
+    EnergyReclaimMin = 0,
+    EnergyReclaimMax = 0,
+    MaxUpAngle = 90,
+    BuildOnGroundOnly = false,
+    SelectEffect = "ui/hud/devices/ui_devices",
+    HasDummy = false,
+}
+
+InsertDeviceBehind("sandbags", suspension)
+
+
+
+table.insert(Sprites, ButtonSprite("hud-suspensionInverted-icon", "HUD/HUD-suspension_inv", nil, ButtonSpriteBottom, nil, nil, path))
+table.insert(Sprites, DetailSprite("hud-detail-suspensionInverted", "suspension_inv", path))
+local suspension2 = DeepCopy(suspension)
+
+if suspension2 then
+    suspension2.SaveName = "suspensionInverted"
+    suspension2.FileName = path .. "/devices/suspensionInverted.lua"
+    suspension2.Icon = "hud-suspensionInverted-icon"
+    suspension2.Detail = "hud-detail-suspensionInverted"
+end
+
+InsertDeviceBehind("suspension", suspension2)
 
 
 
