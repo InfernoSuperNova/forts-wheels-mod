71ad0a5211373b36760d43d0e43a84a522e3d453
Gxaps <36036803+InfernoSuperNova@users.noreply.github.com>
2023-05-24 02:04:38 +1200

    Key bind system

---
+++ scripts/input.lua
@@ -0,0 +1,28 @@
+function OnKey(key, down)
+    --BetterLog(key)
+    if down == true then
+        HeldKeys[key] = true
+    else
+        HeldKeys[key] = nil
+    end
+end
+
+
+
+function CheckHeldKeys()
+    for callback, bind in pairs(Keybinds) do
+        local held = true
+        for _, key in pairs(bind) do
+            if not HeldKeys[key] then held = false end
+        end
+        if held then 
+            if not PressedKeyBinds[callback] then
+                PressedKeyBinds[callback] = true
+            _G[callback]() 
+            end
+        else
+            PressedKeyBinds[callback] = nil
+        end
+    end
+end
+
