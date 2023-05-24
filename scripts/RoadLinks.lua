9cbd3d04ce84bba2a6009e3c228d21657f0bb937
Gxaps <36036803+InfernoSuperNova@users.noreply.github.com>
2023-05-24 22:50:19 +1200

    New road material

--- scripts/RoadLinks.lua
+++ scripts/RoadLinks.lua
@@ -6,14 +6,22 @@ function IndexRoadLinks()
     for k, link in pairs(RoadLinks) do
         local posA = NodePosition(link["nodeA"])
         local posB = NodePosition(link["nodeB"])
-        HighlightCoords({posA, posB})
     end
 end
 
 function PlaceRoadLinksInTable(nodeA, nodeB, linkPos, saveName, deviceId)
     --BetterLog(saveName)
-    if saveName == "armour" then 
+    if saveName == RoadSaveName then 
         table.insert(RoadLinks, {nodeA = nodeA, nodeB = nodeB})
     end
     return true
+end
+
+
+
+function ApplyForceToRoadLinks(nodeA, nodeB, displacement)
+    local newDisplacement = {x = -displacement.x * SpringConst, y = -displacement.y * SpringConst}
+    dlc2_ApplyForce(nodeA, newDisplacement)
+    dlc2_ApplyForce(nodeB, newDisplacement)
+
 end
