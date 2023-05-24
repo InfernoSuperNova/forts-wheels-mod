44606a600cb3c34f0febd4488efbea6e9faf8657
Gxaps <36036803+InfernoSuperNova@users.noreply.github.com>
2023-05-24 22:55:45 +1200

    Road strings

---
+++ materials/strings.lua
@@ -0,0 +1,12 @@
+Merge(Material,
+{
+    RoadLink = L"Road",
+    RoadLinkTip2 = L"Allows wheels to drive on structure, useful for bridges.",
+    RoadLinkTip3 = L"A 'bit' broken at the moment.",
+
+
+
+}
+
+
+)
