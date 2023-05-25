62a0b25b9a252bd5cc437ed402151efa36240d5d
Gxaps <36036803+InfernoSuperNova@users.noreply.github.com>
2023-05-25 22:06:27 +1200

    Strings

--- devices/strings.lua
+++ devices/strings.lua
@@ -8,7 +8,6 @@ Merge(Device,
 
     engine = L"Engine",
     engineTip2 = L"Powers the wheels. Can place multiple engines for more torque.",
-    engineTip3 = L"Throttle can be adjusted when selected. Can be control grouped.",
 
     gearbox = L"Transmission",
     gearboxTip2 = L"Gears for wheels. Place more to add more gears (go faster).",
--- materials/strings.lua
+++ materials/strings.lua
@@ -3,10 +3,4 @@ Merge(Material,
     RoadLink = L"Road",
     RoadLinkTip2 = L"Allows wheels to drive on structure, useful for bridges.",
     RoadLinkTip3 = L"A 'bit' broken at the moment.",
-
-
-
-}
-
-
-)
+})
