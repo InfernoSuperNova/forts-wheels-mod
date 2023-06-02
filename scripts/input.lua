aee8ef627772a93cf892e9daae651e53e41a302e
kumpuu <kumpuu@web.de>
2023-06-02 23:24:00 +0200

    Add hotkeys for left/right/brake
    -Extend keybind manager to call _Up function if exists
    -Add keybinds to config
    -Set gui sliders in callbacks

--- scripts/input.lua
+++ scripts/input.lua
@@ -13,7 +13,10 @@ function CheckHeldKeys()
     for callback, bind in pairs(KEYBINDS) do
         local held = true
         for _, key in pairs(bind) do
-            if not HeldKeys[key] then held = false end
+            if not HeldKeys[key] then
+                held = false
+                break
+            end
         end
         if held then 
             if not PressedKeyBinds[callback] then
@@ -21,7 +24,13 @@ function CheckHeldKeys()
                 _G[callback]() 
             end
         else
-            PressedKeyBinds[callback] = nil
+            if PressedKeyBinds[callback] then
+                PressedKeyBinds[callback] = nil
+
+                if _G[callback .. "_Up"] then
+                    _G[callback .. "_Up"]()
+                end
+            end
         end
     end
 end
