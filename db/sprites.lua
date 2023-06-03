4f82bfaf3a981601e0c0873f2bec3612b9fd74bc
kumpuu <kumpuu@web.de>
2023-06-04 00:02:54 +0200

    Replace brake text button with sprite button

---
+++ db/sprites.lua
@@ -0,0 +1,2 @@
+table.insert(Sprites, ButtonSprite("hud-brake-icon", "HUD/HUD-Brake", nil, ButtonSpriteBottom, true, nil, path) )
+table.insert(Sprites, ButtonSprite("hud-brake-pressed-icon", "HUD/HUD-Brake-pressed", nil, ButtonSpriteBottom, true, nil, path))
