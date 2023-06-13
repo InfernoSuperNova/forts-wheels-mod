9f769bd39b1732c556fd9bc32a0883260589d67f
Gxaps <36036803+InfernoSuperNova@users.noreply.github.com>
2023-06-13 14:56:19 +1200

    UI Localization

---
+++ strings/strings.lua
@@ -0,0 +1,23 @@
+LocalEnglish = {
+    PromptRightClick = "Right click world to close",
+    SpeedText = "Speed: ",
+    TopSpeedText = "Top speed: ",
+    SpeedUnitKmph = " km/h",
+    SpeedUnitMph = " mph",
+    GearText = "Gear: ",
+    PowerText = "Power: ",
+    TrackGroupText = "Set suspension to track group ",
+    WheelGroupText = "Set suspension to wheel",
+}
+LocalChinese = {
+    PromptRightClick = "右键世界关闭界面",
+    SpeedText = "速度: ",
+    TopSpeedText = "最高速度: ",
+    SpeedUnitKmph = " km/h",
+    SpeedUnitMph = " mph",
+    GearText = "档位: ",
+    PowerText = "功率: ",
+    TrackGroupText = "设置悬架绑定组 ",
+    WheelGroupText = "设置车轮悬架",
+}
+Language = {}
