6a04ca6f84fdbe713a88572aeda276e0e51b1ec1
Gxaps <36036803+InfernoSuperNova@users.noreply.github.com>
2023-06-15 15:32:55 +1200

    Move highlight terrain to debug.lua

--- scripts/helpers/debug.lua
+++ scripts/helpers/debug.lua
@@ -123,3 +123,15 @@ function DisableTerrainDebug()
     SetControlText("", "terrainStat3", "")
     SetControlText("", "terrainStat4", "")
 end
+
+function DebugHighlightTerrain(frame)
+    for index, boundary in pairs(data.terrainCollisionBoxes) do
+        if ModDebug.collision == true then
+            local colour1 = { r = 255, g = 255, b = 255, a = 255 }
+            local colour2 = { r = 150, g = 150, b = 150, a = 255 }
+            SpawnCircle(boundary, boundary.r, colour1, 0.04)
+            HighlightPolygon(boundary.square, colour2)
+            HighlightPolygon(Terrain[index], colour2)
+        end
+    end
+end
