ef11fc16be9787b189d882c41dc2801c129e12db
Gxaps <36036803+InfernoSuperNova@users.noreply.github.com>
2023-06-15 15:28:36 +1200

    Implement file list system
    A list of folders, subfolders, and files, recursively dofile'd from a function

---
+++ config/fileList.lua
@@ -0,0 +1,69 @@
+FileList = {
+    ["config"] = {
+        "commanders", --.lua
+        "config",
+        "wheels"
+    },
+
+    ["scripts"] = {
+        ["devices"] = {
+            "devices",
+            "drillScript",
+            "weapons",
+        },
+        ["game"] = {
+            ["wheels"] = {
+                "Propulsion",
+                "Tracks",
+                "WheelCollision",
+            },
+            "coreShield",
+            "editor",
+            "effects",
+            "links",
+            "resources",
+            "terrain",
+        },
+        ["helpers"] = {
+            "BetterLog",
+            "CommanderDetection",
+            "debug",
+            "misc",
+        },
+        ["math"] = {
+            "PID",
+            "VectorFunctions",
+        },
+        ["player"] = {
+            "controls",
+            "input",
+            "localization",
+        },
+    },
+
+    ["strings"] = {
+        "strings"
+    }
+}
+
+local function RGBAtoHex(r, g, b, a, UTF16)
+    local hex = string.format("%02X%02X%02X%02X", r, g, b, a)
+    if UTF16 == true then
+        return L "[HL=" .. towstring(hex) .. L "]"
+    else
+        return "[HL=" .. hex .. "]"
+    end
+end
+function LoadTableFiles(table, path)
+    for folder, item in pairs(table) do
+        if type(item) == "table" then
+            LoadTableFiles(item, path.."/"..folder)
+        else
+            Log("Landcruisers: Loading ".. RGBAtoHex(50, 150, 50, 255, false)..path.."/"..item..".lua")
+            dofile(path .. "/"..item..".lua")
+        end
+    end
+end
+function LoadFiles()
+    LoadTableFiles(FileList, path)
+end
