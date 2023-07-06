1d6a5a4b861680043bf00158f0bd06f23498b3a8
Gxaps <36036803+InfernoSuperNova@users.noreply.github.com>
2023-07-07 11:21:12 +1200

    Reduce torque applied

--- config/wheels.lua
+++ config/wheels.lua
@@ -4,7 +4,7 @@ TRACK_WIDTH = 20                                        --width of the tracks
 WHEEL_SUSPENSION_HEIGHT = {75, 150, 225}                --height from the origin of the suspension to the wheel itself
 SPRING_CONST = 30000                                    --spring constant, the force that wheels will push off with. Higher spring value will necessitate a higher dampening value
 DAMPENING = 3000                                        --spring dampening for wheel collisions (higher is less bouncy, lower is less stable)
-TORQUE_MUL = 10
+TORQUE_MUL = 3
 WHEEL_SAVE_NAME = {"suspension", "suspensionInverted"}  --savenames of the wheel devices
 
 WHEEL_SPRINGS = {
