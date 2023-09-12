015d86d0e78a278c5f9fee4349a5ab3ac19addf1
kumpuu <kumpuu@web.de>
2023-09-12 02:33:37 +0200

    smallui hover sprite

--- db/sprites.lua
+++ db/sprites.lua
@@ -1,6 +1,10 @@
 table.insert(Sprites, ButtonSprite("hud-brake-icon", "HUD/HUD-Brake", nil, ButtonSpriteBottom, true, nil, path) )
 table.insert(Sprites, ButtonSprite("hud-brake-pressed-icon", "HUD/HUD-Brake-pressed", nil, ButtonSpriteBottom, true, nil, path))
 
-table.insert(Sprites, Sprite("hud-smallui-box", path .. "/ui/textures/HUD/HUD-smallui-box.dds"))
+local box = ButtonSprite("hud-smallui-box", "HUD/HUD-smallui-box", nil, ButtonSpriteBottom, true, nil, path)
+box.States.Pressed = nil
+box.States.Disabled = nil
+table.insert(Sprites, box)
+
 table.insert(Sprites, Sprite("hud-smallui-brake", path .. "/ui/textures/HUD/HUD-smallui-brake.dds"))
 table.insert(Sprites, Sprite("hud-smallui-arrow", path .. "/ui/textures/HUD/HUD-smallui-arrow.dds"))
