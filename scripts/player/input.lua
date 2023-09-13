4371611c653c524cdcf398f511e82918267b595c
kumpuu <kumpuu@web.de>
2023-09-13 17:31:49 +0200

    Fix PrintKeybinds()

--- scripts/player/input.lua
+++ scripts/player/input.lua
@@ -31,20 +31,30 @@ function CheckHeldKeys()
     end
 end
 
-function PrintKeybinds(showDebug)
+function PrintKeybinds(showDebug, whitelist)
     local lines = {}
+
+    local in_whitelist = function(name)
+        for k,v in pairs(whitelist) do
+            if name == v then return true end
+        end
+    end
 
     --filter out debug binds and put rest in new table so we can sort alphabetically
     for k,v in pairs(KEYBINDS) do
         if showDebug or not k:lower():find("debug") then
-            table.insert(lines, {name = k, keys = v})
+            if (not whitelist) or in_whitelist(k) then
+                table.insert(lines, {name = k, keys = v})
+            end
         end
     end
 
     table.sort(lines, function(a,b) return a.name < b.name end)
 
     for i,v in ipairs(lines) do
-        local text = v.name:gsub("(%l)(%u)", "%1 %2")  .. ": " --put spaces between words
+        local text = RGBAtoHex(175, 175, 175, 255, false)
+        text = text .. v.name:gsub("(%l)(%u)", "%1 %2")  .. ": " --put spaces between words
+        text = text .. RGBAtoHex(255, 255, 255, 255, false)
 
         for _, cur_key in pairs(v.keys) do
             cur_key = string.gsub(" "..cur_key, "%W%l", string.upper):sub(2) --capitalize first letters
