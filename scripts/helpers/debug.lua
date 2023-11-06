c74caedd12c97804b2cb27ade8cfb747dff973be
Gxaps <36036803+InfernoSuperNova@users.noreply.github.com>
2023-11-07 02:20:13 +1300

    Consume the souls of the damned

--- scripts/helpers/debug.lua
+++ scripts/helpers/debug.lua
@@ -4,6 +4,7 @@
 
 function HighlightCoords(coords)
     for k, coord in pairs(coords) do
+        coord.z = -100
         SpawnCircle(coord, 50, { r = 255, g = 100, b = 100, a = 255 }, data.updateDelta)
     end
 end
