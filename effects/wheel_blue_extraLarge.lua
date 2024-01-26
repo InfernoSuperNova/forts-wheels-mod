4732bbbaf42cbfc36a3850637248c6069a696efc
Gxaps <36036803+InfernoSuperNova@users.noreply.github.com>
2024-01-26 16:48:10 +1300

    Cosmetic wheel effect files

---
+++ effects/wheel_blue_extraLarge.lua
@@ -0,0 +1,27 @@
+
+--frame time, not compatible with mods that change hz rate unfortunately
+LifeSpan = 0.05
+
+Effects =
+{
+
+	{
+		Type = "sprite",
+		TimeToTrigger = 0,
+		LocalPosition = { x = 0, y = 0, z = -0.3 },
+		LocalVelocity = { x = 0, y = 0, z = 0 },
+		Acceleration = { x = 0, y = 0, z = 0 },
+		Drag = 0.0,
+		Sprite = "wheel_blue",
+		Additive = false,
+		TimeToLive = 0.08,
+		Angle = 0,
+		InitialSize = 3.8,
+		ExpansionRate = 0,
+		AngularVelocity = 0,
+		RandomAngularVelocityMagnitude = 0,
+		Colour1 = { 255, 255, 255, 255 },
+		Colour2 = { 255, 255, 255, 255 },
+	},
+
+}
