6b5e94cdd912e452a21633f0298539179367704e
Gxaps <36036803+InfernoSuperNova@users.noreply.github.com>
2024-01-26 16:48:10 +1300

    Cosmetic wheel effect files

---
+++ effects/track_sprocket_Noze_large.lua
@@ -0,0 +1,28 @@
+--frame time, not compatible with mods that change hz rate unfortunately
+LifeSpan = 0.05
+
+
+
+Effects =
+{
+
+	{
+		Type = "sprite",
+		TimeToTrigger = 0,
+		LocalPosition = { x = 0, y = 0, z = -0.1 },
+		LocalVelocity = { x = 0, y = 0, z = 0 },
+		Acceleration = { x = 0, y = 0, z = 0 },
+		Drag = 0.0,
+		Sprite = "NozeSprocket",
+		Additive = false,
+		TimeToLive = 0.08,
+		Angle = 0,
+		InitialSize = 2.6,
+		ExpansionRate = 0,
+		AngularVelocity = 0,
+		RandomAngularVelocityMagnitude = 0,
+		Colour1 = { 255, 255, 255, 255 },
+		Colour2 = { 255, 255, 255, 255 },
+	},
+
+}
---
+++ effects/wheel_Incursus.lua
@@ -0,0 +1,51 @@
+
+--frame time, not compatible with mods that change hz rate unfortunately
+LifeSpan = 0.05
+
+Sprites =
+{
+	{
+		Name = "IncursusWheel",
+
+		States =
+		{
+			Normal =
+			{
+				Frames =
+				{
+					{ texture = path .. "/effects/wheels/Incursus/wheel.png" },
+					
+
+					duration = 0.04,
+					blendColour = false,
+					blendCoordinates = false,
+				},
+				--RandomPlayLength = 2,
+				NextState = "Normal",
+			},
+		},
+	},
+}
+Effects =
+{
+
+	{
+		Type = "sprite",
+		TimeToTrigger = 0,
+		LocalPosition = { x = 0, y = 0, z = -0.3 },
+		LocalVelocity = { x = 0, y = 0, z = 0 },
+		Acceleration = { x = 0, y = 0, z = 0 },
+		Drag = 0.0,
+		Sprite = "IncursusWheel",
+		Additive = false,
+		TimeToLive = 0.08,
+		Angle = 0,
+		InitialSize = 1.3,
+		ExpansionRate = 0,
+		AngularVelocity = 0,
+		RandomAngularVelocityMagnitude = 0,
+		Colour1 = { 255, 255, 255, 255 },
+		Colour2 = { 255, 255, 255, 255 },
+	},
+
+}
