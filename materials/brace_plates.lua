79afa2cbbb2868dd7e1f8ae549506c6f8eb18784
Gxaps <36036803+InfernoSuperNova@users.noreply.github.com>
2024-01-26 16:44:09 +1300

    Add a no cost foundation for auto tank builder

---
+++ materials/brace_plates.lua
@@ -0,0 +1,19 @@
+
+
+
+
+
+table.insert(BracingPlates, {
+    -- roof foundation
+    DisplayName = "No cost foundation",
+    SaveName = "lc-fndfree",
+    Sprite = "foundation0",
+    Sprite1 = "foundation1",
+    Sprite2 = "foundation2",
+    SpriteBG = "foundationBG",
+    Up = { x = 0, y = -1 },
+    EnergyBuildCost = 0,
+    MetalBuildCost = 0,
+    EnergyReclaim = 0,
+    MetalReclaim = 0,
+})
