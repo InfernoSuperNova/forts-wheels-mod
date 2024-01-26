e2df8609461f54ada4e4ab0a35b0d35b257e1ba6
Gxaps <36036803+InfernoSuperNova@users.noreply.github.com>
2024-01-26 16:45:06 +1300

    Implement StructuralHazard for auto tank builder

--- materials/building_materials.lua
+++ materials/building_materials.lua
@@ -12,6 +12,30 @@ for k, v in pairs(Materials) do
     end
 end
 
+
+
+
+FreeNode =
+{
+	Priority = 2,
+	Foundations =
+	{
+--		{ angle = 35, material = "core-fndhvyfloor" }, -- you can have heavier foundations for different angle brackets (lower is a more horizontal surface)
+		{ angle = 145, material = "lc-fndfree" },
+		{ material = "lc-fndfree"},
+	},
+	Plates =
+	{
+		{ material = "core-brc1" },
+--[[
+		{ mass = 75, material = "core-brc1" },
+		{ mass = 125, material = "core-brc2" },
+		{ mass = 200, material = "core-brc3" },
+		{ material = "core-brc4" },
+]]
+	},
+}
+
 table.insert(Sprites, 
     {
 		Name = "road",
@@ -78,6 +102,28 @@ table.insert(Sprites,
 		},
 	}
 )
+table.insert(Sprites, 
+    {
+		Name = "structuralHazard",
+		States =
+		{
+			Normal =
+			{
+				Frames =
+				{
+					-- durations must add up to 1 for the damage keying to work properly
+					-- anything beyond 1 will never show
+					{ texture = path .. "/materials/structuralHazard2.dds", duration = 0.01 },
+					{ texture = path .. "/materials/structuralHazard2.dds", duration = 0.33 },
+					{ texture = path .. "/materials/structuralHazard2.dds", duration = 0.33 },
+					{ texture = path .. "/materials/structuralHazard2.dds", duration = 0.331 },
+					mipmap = true,
+					repeatS = true,
+				},
+			},
+		},
+	}
+)
 local road = DeepCopy(FindMaterial("armour"))
 if road then
     road.SaveName = "RoadLink"
@@ -94,7 +140,9 @@ if structuralAluminium then
 	structuralAluminium.SaveName = "StructuralAluminium"
 	structuralAluminium.Sprite = "structural"
 	structuralAluminium.MetalBuildCost = 0.5
+	structuralAluminium.MetalReclaim = 0.25
 	structuralAluminium.EnergyBuildCost = 0.5
+	structuralAluminium.EnergyReclaim = 0.0
 	structuralAluminium.SupportsDevices = true
 	structuralAluminium.Stiffness = 1000000
 	structuralAluminium.MaxCompression = 0.5
@@ -115,7 +163,9 @@ if structuralAluminiumBackground then
 	structuralAluminiumBackground.SaveName = "StructuralAluminiumBackground"
 	structuralAluminiumBackground.Sprite = "structuralBackground"
 	structuralAluminiumBackground.MetalBuildCost = 0.4
+	structuralAluminiumBackground.MetalReclaim = 0.2
 	structuralAluminiumBackground.EnergyBuildCost = 0.4
+	structuralAluminiumBackground.EnergyReclaim = 0.0
 	structuralAluminiumBackground.SupportsDevices = false
 	structuralAluminiumBackground.CollidesWithFriendlyProjectiles = false
 	structuralAluminiumBackground.CollidesWithEnemyProjectiles = false
@@ -134,4 +184,39 @@ if structuralAluminiumBackground then
 	structuralAluminiumBackground.HitPoints = 100
 	structuralAluminiumBackground.ForegroundTargetSaveName = "StructuralAluminium"
 	table.insert(Materials, structuralAluminiumBackground)
+end
+
+
+local hazardAluminium = DeepCopy(FindMaterial("armour"))
+if hazardAluminium then
+	hazardAluminium.SaveName = "StructuralAluminiumHazard"
+	hazardAluminium.Sprite = "structuralHazard"
+	hazardAluminium.MetalBuildCost = 0.0
+	hazardAluminium.EnergyBuildCost = 0.0
+	hazardAluminium.MetalReclaim = 0
+	hazardAluminium.EnergyReclaim = 0
+	hazardAluminium.SupportsDevices = false
+	hazardAluminium.CollidesWithFriendlyProjectiles = false
+	hazardAluminium.CollidesWithEnemyProjectiles = false
+	hazardAluminium.CollidesWithFriendlyBeams = false
+	hazardAluminium.CollidesWithEnemyBeams = true
+	hazardAluminium.IsBehindDevices = true
+	hazardAluminium.AttachesCladding = true
+	hazardAluminium.BackgroundCladding = true
+	hazardAluminium.CollidesWithWind = false
+	hazardAluminium.FullExtrusion = true
+	hazardAluminium.Stiffness = 800000
+	hazardAluminium.MaxCompression = 0.5
+	hazardAluminium.MaxExpansion = 1.5
+	hazardAluminium.Mass = 0.15
+	hazardAluminium.BuildTime = 0
+	hazardAluminium.ScrapTime = 0
+	hazardAluminium.RequiresFoundationSupport = false
+	hazardAluminium.Node = FreeNode
+	hazardAluminium.HitPoints = 100
+	hazardAluminium.MaxLength = 99999
+	hazardAluminium.MinLength = 0
+
+	hazardAluminium.Enabled = false
+	table.insert(Materials, hazardAluminium)
 end
