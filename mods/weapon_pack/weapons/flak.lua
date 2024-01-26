659daf9a39e6828e72534ad9ee796cf4588f8c78
Gxaps <36036803+InfernoSuperNova@users.noreply.github.com>
2024-01-26 16:50:56 +1300

    Flak behavior changes

--- mods/weapon_pack/weapons/flak.lua
+++ mods/weapon_pack/weapons/flak.lua
@@ -1,4 +1,4 @@
-ReloadTime = ReloadTime / 1.5
+ReloadTime = 0.3
 RoundsEachBurst = 1 
 RoundPeriod = 0.15
 MaxFireAngle = 88
@@ -6,12 +6,16 @@ EnergyFireCost = EnergyFireCost / 3
 MetalFireCost = MetalFireCost / 3
 MinFireSpeed = 12000
 MaxFireSpeed = 12001
-ReloadBank =
-{
-	Max = 10,
-	ReloadTime = 0.3,
-	BaseMult = 1,
-	IndexMult = 0,
-	EnergyMult = 1,
-	MetalMult = 1,
-}
+-- ReloadBank =
+-- {
+-- 	Max = 10,
+-- 	ReloadTime = 0.3,
+-- 	BaseMult = 1,
+-- 	IndexMult = 0,
+-- 	EnergyMult = 1,
+-- 	MetalMult = 1,
+    
+
+
+
+-- }
